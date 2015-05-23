8818c50d37cc2d1913f9c365b34381c42063634b
Tomas Pospisek <tpo_hp@sourcepole.ch>
2015-05-23 21:38:36 +0200

    exit with 0

--- hello.asm
+++ hello.asm
@@ -31,6 +31,7 @@ _loop:
 
 _exit:
     mov     eax,1                               ;system call number (sys_exit)
+    mov     ebx,0                               ;exit value
     int     0x80                                ;call kernel
 
 section     .bss
